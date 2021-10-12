d61541cdcc796f88f56cac6b55a5d1d3f41b30c4
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-10-13 02:06:40 +0600

    auth added

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -15,22 +15,28 @@ step-1) Initial Setup
 4.initialize firebase
 5.Enable auth method
 ----------------------------------------------------------------
-step-2
+step-2 setup component
 1.create login components
 2.create register component
 3.create route for login and register
 
-step-3
+step-3 set auth system
 1.set up sign in method
 2.setup sign out method
 3.user state
 4.special observer (onAuthChanged)
 5.return necessary methods and states from useFirebase() 
 
-step-4
+step-4 create auth context hook (useAuth)
 1.create a auth context 
 2.create context provider
-3.use Auth provider 
-4.create useAuth hook
+3.set  context provider context value
+4.use Auth provider 
+5.create useAuth hook
+----------------------------------------------------------------
+step-5 create private route
+1.create private route 
+2.set private route 
+
 
 */
